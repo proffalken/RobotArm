a184241c867df10dda546555efa6ba3c038beb8d
Matthew Macdonald-Wallace <matt@doics.co>
2025-01-19 19:03:56 +0000

    Do not launch the remote server

--- workspace/src/robotarm_bringup/launch/real_robot.launch.py
+++ workspace/src/robotarm_bringup/launch/real_robot.launch.py
@@ -26,16 +26,7 @@ def generate_launch_description():
             launch_arguments={"is_sim": "False"}.items()
             )
 
-    remote_interface = IncludeLaunchDescription(
-            os.path.join(
-                get_package_share_directory("robotarm_remote"),
-                "launch",
-                "remote_interface.launch.py"
-                )
-            )
-
     return LaunchDescription([
         controller,
         moveit,
-        remote_interface,
         ])
